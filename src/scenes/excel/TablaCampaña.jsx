fe755a1b1e751f892282b36df42f08d503b37714
Enrique Donaire <donaire.q2@gmail.com>
2024-03-20 17:03:17 +0100

    Styles on Table Campaing

--- src/scenes/excel/TablaCampaña.jsx
+++ src/scenes/excel/TablaCampaña.jsx
@@ -10,7 +10,7 @@ const TablaCampaña = () => {
   }, []);
 
   return (
-    <div>
+    <div style={{ margin: "0 auto", maxWidth: "1600px" }}>
       <table>
         <thead>
           <tr>
@@ -28,35 +28,35 @@ const TablaCampaña = () => {
             <th>Neto Fuera</th>
           </tr>
           <tr>
-            <td>Total ACTIVE</td>
-            <td></td>
-            <td></td>
-            <td>1.039.296</td>
-            <td>1.119.878</td>
-            <td>1.120.145</td>
-            <td>1.120.145</td>
-            <td>19.294,04</td>
-            <td>21.242,95</td>
-            <td>21.250,56</td>
-            <td>21.251</td>
-            <td>0</td>
+            <td style={{ border: "none", padding: "4px", textAlign: "right" }}>Total ACTIVE</td>
+            <td style={{ border: "none", padding: "4px", textAlign: "right" }}></td>
+            <td style={{ border: "none", padding: "4px", textAlign: "right" }}></td>
+            <td style={{ border: "none", padding: "4px", textAlign: "right" }}>1.039.296</td>
+            <td style={{ border: "none", padding: "4px", textAlign: "right" }}>1.119.878</td>
+            <td style={{ border: "none", padding: "4px", textAlign: "right" }}>1.120.145</td>
+            <td style={{ border: "none", padding: "4px", textAlign: "right" }}>1.120.145</td>
+            <td style={{ border: "none", padding: "4px", textAlign: "right" }}>19.294,04</td>
+            <td style={{ border: "none", padding: "4px", textAlign: "right" }}>21.242,95</td>
+            <td style={{ border: "none", padding: "4px", textAlign: "right" }}>21.250,56</td>
+            <td style={{ border: "none", padding: "4px", textAlign: "right" }}>21.251</td>
+            <td style={{ border: "none", padding: "4px", textAlign: "right" }}>0</td>
           </tr>
         </thead>
         <tbody>
           {data.map((item, index) => (
             <tr key={index}>
-              <td>{item["Placement Status"]}</td>
-              <td>{item["Campaign Name"]}</td>
-              <td>{item["Fecha"]}</td>
-              <td>{item["Impre.Objetivo"]}</td>
-              <td>{item["Impre.servidas"]}</td>
-              <td>{item["Impresiones previstas a servir"]}</td>
-              <td>{item["Impression Real/Prev"]}</td>
-              <td>{item["Importe objetivo"]}</td>
-              <td>{item["Importe real"]}</td>
-              <td>{item["Importe previsto a servir"]}</td>
-              <td>{item["Importe Real/Prev"]}</td>
-              <td>{item["Neto Fuera"]}</td>
+              <td style={{ border: "none", padding: "4px", textAlign: "right" }}>{item["Placement Status"]}</td>
+              <td style={{ border: "none", padding: "4px", textAlign: "right" }}>{item["Campaign Name"]}</td>
+              <td style={{ border: "none", padding: "4px", textAlign: "right" }}>{item["Fecha"]}</td>
+              <td style={{ border: "none", padding: "4px", textAlign: "right" }}>{item["Impre.Objetivo"]}</td>
+              <td style={{ border: "none", padding: "4px", textAlign: "right" }}>{item["Impre.servidas"]}</td>
+              <td style={{ border: "none", padding: "4px", textAlign: "right" }}>{item["Impresiones previstas a servir"]}</td>
+              <td style={{ border: "none", padding: "4px", textAlign: "right" }}>{item["Impression Real/Prev"]}</td>
+              <td style={{ border: "none", padding: "4px", textAlign: "right" }}>{item["Importe objetivo"]}</td>
+              <td style={{ border: "none", padding: "4px", textAlign: "right" }}>{item["Importe real"]}</td>
+              <td style={{ border: "none", padding: "4px", textAlign: "right" }}>{item["Importe previsto a servir"]}</td>
+              <td style={{ border: "none", padding: "4px", textAlign: "right" }}>{item["Importe Real/Prev"]}</td>
+              <td style={{ border: "none", padding: "4px", textAlign: "right" }}>{item["Neto Fuera"]}</td>
             </tr>
           ))}
         </tbody>
